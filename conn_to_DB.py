7cf8f49ac9d7726abc2e5cbdb1b632c3125d55f6
TAsUjxnMIL <sujan_10@hotmail.de>
2021-04-22 09:18:06 +0200

    first commit

--- conn_to_DB.py
+++ conn_to_DB.py
@@ -2,8 +2,8 @@ from influxdb import InfluxDBClient
 
 def setContoInflux():
     #Setup database 
-    client = InfluxDBClient('localhost', 8086, 'admin', 'Password1', 'CoronaDB_test')
-    client.create_database('CoronaDB_test')
+    client = InfluxDBClient('localhost', 8086, 'admin', 'Password1', 'CoronaDB_test1')
+    client.create_database('CoronaDB_test1')
     client.get_list_database()
-    client.switch_database('CoronaDB_test')
+    client.switch_database('CoronaDB_test1')
     return client
