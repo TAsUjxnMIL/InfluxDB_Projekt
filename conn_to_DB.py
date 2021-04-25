92607c036307210b20f2b9f0ec53b23369c68b47
TAsUjxnMIL <sujan_10@hotmail.de>
2021-04-25 19:39:11 +0200

    connection updated

--- conn_to_DB.py
+++ conn_to_DB.py
@@ -1,32 +1,5 @@
 from influxdb import InfluxDBClient
 
-# def set_Con_to_Influx():
-#     """set con to influx
-#         set connection to Influx database
-        
-#         Args:
-#             none
-
-#         Returns:
-#             client  (InfluxDBClient object): client contains to connection to the db, queries are executed over this object
-        
-#         Test:
-#             * is user input correct?
-#             * are the default values taken? 
-#     """
-#     try:
-#         client = InfluxDBClient('localhost', 8086, 'admin', 'Password1', 'CoronaDB_World_test')
-#         client.ping()
-    
-#     except Exception as general_error:
-#         print("There is a mistake in the arguments of InfluxDBClient: " + str(general_error)) 
-#         return 
-    
-#     client.create_database('CoronaDB_World_test')
-#     client.get_list_database()
-#     client.switch_database('CoronaDB_World_test')
-#     return client
-
 class MyInfluxDBClient:
     def __init__(self, _localhost='localhost', _port=8086, _username='admin', _password='Password1', _database='CoronaDB_World_test'):
         self.localhost = _localhost
@@ -37,6 +10,19 @@ class MyInfluxDBClient:
         self.connection = self.setContoInflux()
 
     def setContoInflux(self):
+        """set con to influx
+           set connection to Influx database
+        
+            Args:
+              none
+
+            Returns:
+                client  (InfluxDBClient object): client contains to connection to the db, queries are executed over this object
+        
+            Test:
+                * is user input correct?
+                * are the default values taken? 
+        """
         #Setup database 
         try:
             client = InfluxDBClient(self.localhost, self.port, self.username, self.password, self.database)
